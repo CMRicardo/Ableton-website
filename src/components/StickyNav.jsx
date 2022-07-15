5d937df09fc76ab2958682ae85b4f1c22f28c024
Ricardo Corrales <ricardocorrales84@gmail.com>
2022-07-15 09:47:20 -0600

    Added ul tag on the sticky navbar

--- src/components/StickyNav.jsx
+++ src/components/StickyNav.jsx
@@ -1,9 +1,19 @@
-export function StickyNav () {
+export function StickyNav() {
   return (
-    <nav className='flex list-none gap-8 py-4 px-10 font-bold text-sm sticky top-0 bg-white opacity-80 z-20'>
-      <li><a href='#' className='text-[#fd5948]'>About</a></li>
-      <li><a href='#'>Jobs</a></li>
-      <li><a href='#'>Apprenticeship</a></li>
+    <nav className='py-4 px-10 font-bold text-sm sticky top-0 bg-white opacity-80 z-20'>
+      <ul className='flex list-none gap-8'>
+        <li>
+          <a href='#' className='text-[#fd5948]'>
+            About
+          </a>
+        </li>
+        <li>
+          <a href='#'>Jobs</a>
+        </li>
+        <li>
+          <a href='#'>Apprenticeship</a>
+        </li>
+      </ul>
     </nav>
   )
 }
